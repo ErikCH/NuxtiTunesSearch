18da77e45a219b597dbbff1d747346a4d7825065
Erik Hanchett <erik.hanchett@gmail.com>
2017-12-21 21:22:51 -0800

    updated search

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>Hello From results route {{$route.params.id}}</h1>
+    <h2> output</h2>
+    {{albumData}}
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  asyncData({params}) {
+    return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+      .then((response) => {
+        return {albumData: response.data.results}
+      });
+  },
+  middleware: 'search'
+
+}
+</script>
